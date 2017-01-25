e350d25c507673dcef9a56f8fb0d35bce3026727
mrMekdad <mekdadr6@gmail.com>
2017-01-25 22:32:00 +0100

    signal-sketchbook: add repository metadata and packaging

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="reda-2017-signal-sketchbook",
+    version="0.1.0",
+    description="Signal plotting and quick analysis notebook replacement built with standard Python only.",
+    author="Red@",
+    packages=["signal_sketchbook"],
+    package_dir={"": "src"},
+)
